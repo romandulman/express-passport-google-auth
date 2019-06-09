947576a147e1ac4de8054216ffa410a47fccbc12
Roman Dulman <romandulman@gmail.com>
2019-06-09 22:43:45 +0300

    first commit

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,16 +1,17 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const keys = require('./keys');
 
 
 passport.use(
     new GoogleStrategy({
-        callbackURL:'/auth/google/redirect',
-        clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret
 
-    }, (accessToken,refreshToken,profile,done) =>{
-      //  console.log(profile);
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
+        callbackURL:'http://localhost:3000/auth/google/redirect'
+
+    }, (accessToken, refreshToken, profile, done) =>{
+       console.log(profile);
         console.log('ok')
     })
 
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,19 +2,26 @@ const router = require('express').Router();
 const passport = require('passport');
 
 //login
-router.get('/login',(req,res)=>{
-    res.render('login',{ title: 'Login' })
+router.get('/login', (req, res) => {
+    res.render('login', {title: 'Login'})
 });
 
-//auth with google
-router.get('/google',passport.authenticate('google',{
-    scope:['profile']
-}));
-router.get('/google/redirect',passport.authenticate('google'),(req,res)=>{
-   res.send('Logged in with Google')
-});
 
-router.get('/logout',(req,res)=>{
+router.get('/google',
+    passport.authenticate('google', {
+        scope: ['https://www.googleapis.com/auth/plus.login']
+
+    }));
+
+
+router.get('/google/redirect',
+    passport.authenticate('google', {failureRedirect: '/login'}),
+    function (req, res) {
+        res.redirect('/');
+    });
+
+
+router.get('/logout', (req, res) => {
 
 });
 
